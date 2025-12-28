569576e26958d1cfee66b9453df1bc76d5f0e585
syamsul123arifin <3syamsul333.com>
2025-12-28 10:26:13 +0700

    Revert commit f80c362

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget { 
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
